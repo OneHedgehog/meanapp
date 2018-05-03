bbc4c9c0411325ec757555348e3a20cb92682f91
OneHedgehog <aperteda1@gmail.com>
2018-05-03 12:53:57 +0300

    mlab added

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/mean-db',
+    uri: 'mongodb://OneHedgehog:s583jqv.@ds111410.mlab.com:11410/meanapp',
     secret: crypto,
     db: 'mean-db'
 };
