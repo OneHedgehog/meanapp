1f5da25e72fabb78a3f628aeba1974dfe30dafdd
Your Name <you@example.com>
2018-05-06 01:13:35 +0300

    make it better mor

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname + '/client/dist/'));
 });
 
-app.listen(5000, ()=> {
+app.listen( process.env.PORT || 5000, ()=> {
     console.log('listend')
 
 });
